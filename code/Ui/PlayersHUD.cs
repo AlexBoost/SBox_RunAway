ee7185a1baab719ae68f6ce1621165d3af51dbf8
Asklop <56790068+Asklop@users.noreply.github.com>
2021-10-17 15:09:07 +0200

    Add files via upload

---
+++ code/Ui/PlayersHUD.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using RunAway.Players;
+
+namespace RunAway.Ui
+{
+	public class PlayersHUD : Panel
+	{
+		public Label playerClass;
+		public Label subBox;
+		public PlayersHUD()
+		{
+			playerClass = Add.Label( "", "value" );
+			subBox = Add.Label( "", "subBox" );
+		}
+
+		public override void Tick()
+		{
+			var player = Local.Client;
+			if ( player == null ) return;
+			playerClass.Text = RunAway_Game.PlayerClass;
+		}
+	}
+}
---
+++ code/Ui/ScoreBoard.cs
@@ -0,0 +1,25 @@
+﻿using Sandbox;
+using Sandbox.Hooks;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using System;
+using System.Collections.Generic;
+
+namespace RunAway.Ui
+{
+	class ScoreBoard : Panel
+	{
+		public Label scoreboard;
+		public ScoreBoard()
+		{
+			scoreboard = Add.Label("", "text");
+		}
+
+		public override void Tick()
+		{
+			base.Tick();
+
+			SetClass( "open", Input.Down( InputButton.Score ) );
+		}
+	}
+}
